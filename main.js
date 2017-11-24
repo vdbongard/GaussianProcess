3df90cdbd10eef66301757e15977632d0e6a4118
Daniel van den Bongard <daniel@vdbongard.com>
2017-11-24 20:10:58 +0100

    Fix: previous states of mean, sd95 and samples were not removed on the graph

--- main.js
+++ main.js
@@ -6,7 +6,7 @@ class GaussianProcess {
     this.yTop = 3
     this.yBottom = -3
     this.covarianceMatrix = []
-    this.graph = new Graph(this.xLeft, this.xRight, this.yTop, this.yBottom)
+    this.graph = new Graph(this.xLeft, this.xRight, this.yTop, this.yBottom, this.steps)
     this.testingPointsX = numeric.linspace(this.xLeft, this.xRight, this.steps)
     this.trainingPointsX = []
     this.trainingPointsY = []
@@ -19,32 +19,25 @@ class GaussianProcess {
 
   init () {
     this.initCovarianceMatrix()
-    this.computeProjection(0, 0, 0)
-    this.drawMeanAndInterval()
+    this.computeProjection()
+    this.graph.drawMeanAndInterval(this.mu, this.sd95)
     this.sample()
 
     d3.select('body').append('button').text('Sample').on('click', this.sample.bind(this))
+    d3.select('body').append('button').text('Reset Samples').on('click', this.graph.resetSamples.bind(this.graph))
 
     document.querySelector('svg').addEventListener('click', this.addTrainingPoint.bind(this))
   }
 
   initCovarianceMatrix () {
-    const dm = GaussianProcess.computeDistanceMatrix(this.testingPointsX, this.testingPointsX)
-    this.covarianceMatrix = GaussianProcess.squaredExponentialKernel(dm)
-
-    console.log('Covariance Matrix: ', this.covarianceMatrix)
-  }
-
-  drawMeanAndInterval () {
-    this.graph.drawLine(this.mu)
-    this.graph.drawLine(numeric.add(this.mu, this.sd95))
-    this.graph.drawLine(numeric.sub(this.mu, this.sd95))
+    const distanceMatrix = GaussianProcess.computeDistanceMatrix(this.testingPointsX, this.testingPointsX)
+    this.covarianceMatrix = GaussianProcess.squaredExponentialKernel(distanceMatrix)
   }
 
   sample () {
-      const z = GaussianProcess.randomNormalArray(this.steps)
-      const dataY = numeric.add(numeric.dot(this.proj, z), this.mu)
-      this.graph.drawLine(dataY, false, false, true)
+    const z = GaussianProcess.randomNormalArray(this.steps)
+    const dataY = numeric.add(numeric.dot(this.proj, z), this.mu)
+    this.graph.drawSample(dataY)
   }
 
   addTrainingPoint (event) {
@@ -59,16 +52,15 @@ class GaussianProcess {
 
       this.computeProjection(dmTr, dmTeTr, this.trainingPointsY)
       this.graph.drawPoint(graphPoint.x, graphPoint.y)
-      this.drawMeanAndInterval()
-      this.sample()
+      this.graph.drawMeanAndInterval(this.mu, this.sd95)
     }
   }
 
   static computeDistanceMatrix (xData1, xData2) {
     const distanceMatrix = GaussianProcess.initTwoDimensionalArray(xData1.length, xData2.length)
 
-    for (let i = 0; i < xData1.length; i++){
-      for (let j = 0; j < xData2.length; j++){
+    for (let i = 0; i < xData1.length; i++) {
+      for (let j = 0; j < xData2.length; j++) {
         distanceMatrix[i][j] = Math.abs(xData2[j] - xData1[i])
       }
     }
@@ -76,45 +68,47 @@ class GaussianProcess {
     return distanceMatrix
   }
 
-  computeProjection(dmTr, dmTeTr, trY) {
+  computeProjection (dmTr, dmTeTr, trY) {
     const Mtr = numeric.dim(dmTr)[0]
     const Mte = this.steps
 
     if (Mtr > 0) {
-      const Kxx = GaussianProcess.squaredExponentialKernel(dmTr)
+      const kxx = GaussianProcess.squaredExponentialKernel(dmTr)
 
-      for (let i = 0; i < Mtr; i++){
-        Kxx[i][i] += 0.2;
+      for (let i = 0; i < Mtr; i++) {
+        kxx[i][i] += 0.2
       }
 
-      const KxxSvd = numeric.svd(Kxx)
+      const svdKxx = numeric.svd(kxx)
 
-      for (let i = 0; i < Mtr; i++){
-        if (KxxSvd.S[i] > numeric.epsilon){
-          KxxSvd.S[i] = 1.0/KxxSvd.S[i];
+      for (let i = 0; i < Mtr; i++) {
+        if (svdKxx.S[i] > numeric.epsilon) {
+          svdKxx.S[i] = 1.0 / svdKxx.S[i]
         } else {
-          KxxSvd.S[i] = 0.0;
+          svdKxx.S[i] = 0.0
         }
       }
 
-      const Kx = GaussianProcess.squaredExponentialKernel(dmTeTr)
+      const kx = GaussianProcess.squaredExponentialKernel(dmTeTr)
 
-      const tmp = numeric.dot(Kx, KxxSvd.U);
+      const tmp = numeric.dot(kx, svdKxx.U)
 
-      // there seems to be a bug in numeric.svd: svd1.U and transpose(svd1.V) are not always equal for a symmetric matrix
-      this.mu = numeric.dot(tmp, numeric.mul(KxxSvd.S, numeric.dot(numeric.transpose(KxxSvd.U), trY)));
-      let cov = numeric.dot(tmp, numeric.diag(numeric.sqrt(KxxSvd.S)));
-      cov = numeric.dot(cov, numeric.transpose(cov));
-      cov = numeric.sub(this.covarianceMatrix, cov);
-      const covSvd = numeric.svd(cov);
-      for (let i = 0; i < Mte; i++){
-        if (covSvd.S[i] < numeric.epsilon){
-          covSvd.S[i] = 0.0;
+      this.mu = numeric.dot(tmp, numeric.mul(svdKxx.S, numeric.dot(numeric.transpose(svdKxx.U), trY)))
+
+      let cov = numeric.dot(tmp, numeric.diag(numeric.sqrt(svdKxx.S)))
+      cov = numeric.dot(cov, numeric.transpose(cov))
+      cov = numeric.sub(this.covarianceMatrix, cov)
+
+      const svdCov = numeric.svd(cov)
+
+      for (let i = 0; i < Mte; i++) {
+        if (svdCov.S[i] < numeric.epsilon) {
+          svdCov.S[i] = 0.0
         }
       }
-      this.proj = numeric.dot(covSvd.U, numeric.diag(numeric.sqrt(covSvd.S)));
-      this.sd95 = numeric.mul(1.98, numeric.sqrt(numeric.getDiag(numeric.dot(this.proj, numeric.transpose(this.proj)))));
 
+      this.proj = numeric.dot(svdCov.U, numeric.diag(numeric.sqrt(svdCov.S)))
+      this.sd95 = numeric.mul(1.98, numeric.sqrt(numeric.getDiag(numeric.dot(this.proj, numeric.transpose(this.proj)))))
     } else {
       this.mu = numeric.rep([this.steps], 0)
       this.sd95 = numeric.mul(1.98, numeric.sqrt(numeric.getDiag(this.covarianceMatrix)))
@@ -123,7 +117,7 @@ class GaussianProcess {
     }
   }
 
-  static initTwoDimensionalArray(dim1, dim2) {
+  static initTwoDimensionalArray (dim1, dim2) {
     const array = new Array(dim1)
     for (let i = 0; i < dim1; i++) {
       array[i] = new Array(dim2)
@@ -148,7 +142,7 @@ class GaussianProcess {
 }
 
 class Graph {
-  constructor (xLeft, xRight, yTop, yBottom) {
+  constructor (xLeft, xRight, yTop, yBottom, steps) {
     this.marginAll = 50
     this.margin = {top: this.marginAll, right: this.marginAll, bottom: this.marginAll, left: this.marginAll}
     this.width = window.innerWidth - this.margin.left - this.margin.right
@@ -157,6 +151,7 @@ class Graph {
     this.xRight = xRight
     this.yTop = yTop
     this.yBottom = yBottom
+    this.linSpace = numeric.linspace(this.xLeft, this.xRight, steps)
     this.xScale = d3.scaleLinear().domain([this.xLeft, this.xRight]).range([0, this.width])
     this.yScale = d3.scaleLinear().domain([this.yBottom, this.yTop]).range([this.height, 0])
     this.svg = d3.select('body').append('svg')
@@ -164,7 +159,11 @@ class Graph {
       .attr('height', this.height + this.margin.top + this.margin.bottom)
       .append('g')
       .attr('transform', 'translate(' + this.margin.left + ',' + this.margin.top + ')')
-
+    this.meanAndIntervalGroup = this.svg.append('g')
+    this.sd95 = this.meanAndIntervalGroup.append('path').attr('class', 'sd95-area')
+    this.mean = this.meanAndIntervalGroup.append('path').attr('class', 'mean-line')
+    this.showDots = false
+    this.sampleGroup = this.svg.append('g')
     this.lines = []
     this.lineDots = []
 
@@ -182,43 +181,60 @@ class Graph {
       .call(d3.axisLeft(this.yScale))
   }
 
-  drawLine (data, showDots, resetLines, randomColors) {
-    const linSpace = numeric.linspace(this.xLeft, this.xRight, data.length)
-    const dataSet = d3.range(data.length).map(function (i) { return {'y': data[i]} })
-    const randomColor = randomColors ? 'hsl(' + Math.random() * 360 + ',100%,50%)' : '#ccc'
+  drawMeanAndInterval (mu, sd95) {
+    const line = d3.line()
+      .x((d, i) => this.xScale(this.linSpace[i]))
+      .y(d => this.yScale(d))
 
-    // if (resetLines) {
-    //   this.lines.forEach(line => line.remove())
-    //   this.lineDots.forEach(dots => dots.remove())
-    // }
+    const area = d3.area()
+      .x((d, i) => this.xScale(this.linSpace[i]))
+      .y0(d => this.yScale(d.mu - d.sd95))
+      .y1(d => this.yScale(d.mu + d.sd95))
+
+    const dataSet = d3.range(mu.length).map((i) => { return {mu: mu[i], sd95: sd95[i]} })
+
+    this.mean.attr('d', line(mu))
+    this.sd95.attr('d', area(dataSet))
+
+    this.resetSamples()
+  }
+
+  drawSample (data) {
+    const randomColor = 'hsl(' + Math.random() * 360 + ',100%,50%)'
 
     const line = d3.line()
-      .x((d, i) => { return this.xScale(linSpace[i]) })
-      .y((d) => { return this.yScale(d.y) })
+      .x((d, i) => this.xScale(this.linSpace[i]))
+      .y(d => this.yScale(d))
+
+    const currentGroup = this.sampleGroup.append('g')
 
     this.lines.push(
-      this.svg.append('path')
-        .datum(dataSet)
-        .attr('class', 'line')
+      currentGroup.append('path')
+        .attr('class', 'sample-line')
         .attr('stroke', randomColor)
-        .attr('d', line)
+        .attr('d', line(data))
     )
 
-    if (showDots) {
+    if (this.showDots) {
       this.lineDots.push(
-        this.svg.selectAll('.dot')
-          .data(dataSet)
+        currentGroup.append('g').selectAll('.dot')
+          .data(data)
           .enter().append('circle')
           .attr('class', 'dot')
-          .attr('cx', (d, i) => { return this.xScale(linSpace[i]) })
-          .attr('cy', (d) => { return this.yScale(d.y) })
-          .attr('r', 3)
+          .attr('cx', (d, i) => this.xScale(this.linSpace[i]))
+          .attr('cy', d => this.yScale(d))
+          .attr('r', 2)
           .attr('fill', randomColor)
       )
     }
   }
 
-  toLocal(x, y) {
+  resetSamples () {
+    this.lines.forEach(line => line.remove())
+    this.lineDots.forEach(dots => dots.remove())
+  }
+
+  toLocal (x, y) {
     // check bounds
     if (x < this.margin.left
       || x > this.margin.left + this.width
@@ -231,7 +247,7 @@ class Graph {
     const newX = (x - this.margin.left) / this.width * xRange + this.xLeft
     const newY = -((y - this.margin.top) / this.height * yRange + this.yBottom)
 
-    return { x: newX, y: newY }
+    return {x: newX, y: newY}
   }
 
   drawPoint (x, y) {
